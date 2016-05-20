7e74faf939e768d6425bef4e8dd473c88d8e8fc8
Alex <alexcris.m@gmail.com>
2016-05-19 20:41:07 -0500

    .

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
-
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
@@ -9,19 +6,17 @@ var config = require('config');
 var api = new ParseServer({
   databaseURI: process.env.MONGODB_URI,
   cloud: path.join(__dirname, '/cloud/main.js'),
-  appId: 'pitagoras',
-  appName: 'pitagoras',
-  masterKey: process.env.MASTER_KEY || 'master', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://pitagoras-server.herokuapp.com/pitagoras'  // Don't forget to change to https if needed
+  appId: process.env.APP_ID,
+  appName: process.env.APP_NAME,
+  masterKey: process.env.MASTER_KEY,
+  serverURL: process.env.SERVER_URL
 });
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
-
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/pitagoras';
@@ -29,17 +24,11 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website. Please star the parse-server repo on GitHub!');
-});
-
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+  res.status(200).send('Pitagoras Parse Server');
 });
 
 var port = process.env.PORT || 1337;
 
 app.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log('pitagoras-server running on port ' + port + '.');
 });
